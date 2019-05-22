c1c88916eb9efa2452ef4b254675440d1a7bb364
kai.ren <kairen1987@outlook.com>
2019-05-22 10:55:58 +0800

    veemacc.mm fix

--- riscv/insns/veemacc_mm.h
+++ riscv/insns/veemacc_mm.h
@@ -5,9 +5,10 @@ struct ShapeStride sst;
 sst_fill(sst);
 unsigned long rs1 = MMU.get_phy_addr(RS1);
 unsigned long rs2 = MMU.get_phy_addr(RS2);
-unsigned long rd = MMU.get_phy_addr(RD);
+unsigned long rd  = MMU.get_phy_addr(RD);
+
 if(DIM == 0){
-	CusIns.veemacc_mm((half*)rs1, (half*)rd, (half*)rs2, &sst);
+	CusIns.veemacc_mm((half*)rs1, (half*)&FRD.v, (half*)rs2, &sst);
 }
 else{
 	CusIns.veemacc_mm((half*)rs1, (half*)rd, (half*)rs2, &sst, DIM_DM);
