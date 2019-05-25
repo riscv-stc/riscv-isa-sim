2580569cf0efd083536d91993db5a045faa2fdab
kai.ren <kai.ren@streamcomputing.com>
2019-05-25 11:49:00 +0800

    fix vecvt ad velut

--- riscv/insns/vecvt_hf_x16_m.h
+++ riscv/insns/vecvt_hf_x16_m.h
@@ -1,1 +1,9 @@
 require_extension('V');
+
+class CustomInsns CusIns;
+struct ShapeStride sst;
+sst_fill(sst);
+unsigned long rs1 = MMU.get_phy_addr(RS1);
+unsigned long rd = MMU.get_phy_addr(RD);
+
+CusIns.vecvt_hf_x16_m((int16_t*)rs1, (half*)rd, &sst);
--- riscv/insns/velut_m.h
+++ riscv/insns/velut_m.h
@@ -4,6 +4,7 @@ class CustomInsns CusIns;
 struct ShapeStride sst;
 sst_fill(sst);
 unsigned long rs1 = MMU.get_phy_addr(RS1);
+unsigned long rs2 = MMU.get_phy_addr(RS2);
 unsigned long rd = MMU.get_phy_addr(RD);
 
-CusIns.velut_m((uint16_t*)rs1, RS2, (half*)rd, &sst);
+CusIns.velut_m((uint16_t*)rs1, (uint16_t*)rs2, (half*)rd, &sst);
