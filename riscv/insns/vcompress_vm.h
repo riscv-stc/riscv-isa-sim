f76da2155625f971e79396778a40c7ec77bded59
hongfeng.chen <hongfeng.chen@streamcomputing.com>
2019-06-01 13:22:56 +0800

    add vcompress.vm ins

---
+++ riscv/insns/vcompress_vm.h
@@ -0,0 +1,26 @@
+require_extension('V');
+
+check_vstart{
+	int vd_idx = 0;
+        switch(SEW) {
+                case 8:
+			vector_for_each(idx){
+				if (VRS1.vb[idx] & 0x1) {
+				    WRITE_VRD_B(VRS2.vb[idx], vd_idx++);
+				}
+			}
+                break;
+
+                case 16:
+			vector_for_each(idx){
+				if (VRS1.vh[idx] & 0x1) {
+				    WRITE_VRD_H(VRS2.vh[idx], vd_idx++);
+				}
+			}
+                break;
+
+                default:
+                break;
+        }
+}
+
--- spike_main/disasm.cc
+++ spike_main/disasm.cc
@@ -377,6 +377,7 @@ disassembler_t::disassembler_t(int xlen)
   #define DEFINE_SV14TYPE(code) DISASM_INSN(#code, code, 0, {&vrd, &frs1, &vrs2, &vm})
   #define DEFINE_SV15TYPE(code) DISASM_INSN(#code, code, 0, {&frd, &vrs2})
   #define DEFINE_SV16TYPE(code) DISASM_INSN(#code, code, 0, {&vrd, &vrs2, &vm})
+  #define DEFINE_SV17TYPE(code) DISASM_INSN(#code, code, 0, {&vrd, &vrs2, &vrs1})
   #define DEFINE_ITYPE(code) DISASM_INSN(#code, code, 0, {&xrd, &xrs1, &imm})
   #define DEFINE_ITYPE_SHIFT(code) DISASM_INSN(#code, code, 0, {&xrd, &xrs1, &shamt})
   #define DEFINE_I0TYPE(name, code) DISASM_INSN(name, code, mask_rs1, {&xrd, &imm})
@@ -786,6 +787,7 @@ disassembler_t::disassembler_t(int xlen)
   DEFINE_SV6TYPE(vrgather_vv);
   DEFINE_SV12TYPE(vrgather_vx);
   DEFINE_SV11TYPE(vrgather_vi);
+  DEFINE_SV17TYPE(vcompress_vm);
 
 
 
