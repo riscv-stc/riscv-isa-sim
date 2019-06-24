6a50bbd6481e590aeddb132f5ecdfab80e935472
hongfeng.chen <hongfeng.chen@streamcomputing.com>
2019-06-24 15:29:38 +0800

    stride of x8/xu8 should not right shift

--- riscv/insns/vecvt_hf_xu8_m.h
+++ riscv/insns/vecvt_hf_xu8_m.h
@@ -6,4 +6,5 @@ sst_fill(sst);
 unsigned long rs1 = MMU.get_phy_addr(RS1);
 unsigned long rd = MMU.get_phy_addr(RD);
 
+sst.stride_rs1 = STRIDE_RS1;
 CusIns.vecvt_hf_xu8_m((uint8_t*)rs1, (half*)rd, &sst);
