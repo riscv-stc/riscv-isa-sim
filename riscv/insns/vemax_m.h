ee2ecf18703569626f91d56fcbb75e074292cca4
hongfeng.chen <hongfeng.chen@streamcomputing.com>
2019-06-27 11:57:37 +0800

    write f32 th frd

--- riscv/insns/vemax_m.h
+++ riscv/insns/vemax_m.h
@@ -6,9 +6,11 @@ sst_fill(sst);
 unsigned long rs1 = MMU.get_phy_addr(RS1);
 unsigned long rs2 = MMU.get_phy_addr(RS2);
 unsigned long rd = MMU.get_phy_addr(RD);
+float16_t f16;
 
 if(DIM == 0){
-	CusIns.vemax_m((half*)rs1, (half*)&FRD.v, &sst);
+	CusIns.vemax_m((half*)rs1, (half*)&f16.v, &sst);
+	WRITE_FRD(f16_to_f32(f16));
 }
 else{
 	CusIns.vemax_m((half*)rs1, (half*)rd, &sst, DIM_DM);
