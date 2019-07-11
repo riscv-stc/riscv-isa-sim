0a61a1c355e3f8f989018237e00b245e754cbd22
hongfeng.chen <hongfeng.chen@streamcomputing.com>
2019-07-11 07:07:26 -0400

    update veacc insn rd half to float.

--- riscv/insns/veemacc_mm.h
+++ riscv/insns/veemacc_mm.h
@@ -6,9 +6,11 @@ sst_fill(sst);
 unsigned long rs1 = MMU.get_phy_addr(RS1);
 unsigned long rs2 = MMU.get_phy_addr(RS2);
 unsigned long rd  = MMU.get_phy_addr(RD);
+float16_t f16;
 
 if(DIM == 0){
-	CusIns.veemacc_mm((half*)rs1, (half*)&FRD.v, (half*)rs2, &sst);
+	CusIns.veemacc_mm((half*)rs1, (half*)&f16.v, (half*)rs2, &sst);
+	WRITE_FRD(f16_to_f32(f16));
 }
 else{
 	CusIns.veemacc_mm((half*)rs1, (half*)rd, (half*)rs2, &sst, DIM_DM);
