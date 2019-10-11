9b1b466dba6d9e578d154a764e7fbe0abc59b908
wenchu.yang <wenchu.yang@streamcomputing.com>
2019-10-11 19:05:42 +0800

    update pld insn.

--- riscv/insns/pld.h
+++ riscv/insns/pld.h
@@ -20,8 +20,8 @@ for (int core_id = 0; core_id < 32; core_id++)
 {
     if (core_map & (0x1 << core_id)) {
         for (int i = 0; i < row; i++)
-	    proxy->tcpXfer(0, core_id, dst + i * col * 2, src + i * stride, \
-			    col * 2, 0, Transport::AbstractProxy::LLB2CORE);
+            proxy->tcpXfer(0, core_id, dst + i * col * 2, 0, col * 2,   \
+                src + i * stride, Transport::AbstractProxy::LLB2CORE);
     }
 }
 
