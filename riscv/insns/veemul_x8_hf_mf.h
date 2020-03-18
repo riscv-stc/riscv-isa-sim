0f6ec142db889c86a988d476c40f8e559bde0aef
wenchu.yang <wenchu.yang@streamcomputing.com>
2020-03-18 11:29:53 +0800

    insn veemul_x8_hf_mf rd stride no need x2.

--- riscv/insns/veemul_x8_hf_mf.h
+++ riscv/insns/veemul_x8_hf_mf.h
@@ -3,10 +3,14 @@ NCP_AUNIT();
 
 class CustomInsns CusIns;
 struct ShapeStride sst;
+
 sst_fill(sst);
+sst.stride_rd = STRIDE_RD;
+
 unsigned long rs1 = MMU.get_phy_addr(RS1);
 unsigned long rd = MMU.get_phy_addr(RD);
 half vr2;
 vr2.x = f32_to_f16(f32(FRS2)).v;
 
 CusIns.veemul_x8_hf_mf((half *)rs1, (int8_t *)rd, vr2, &sst);
+
