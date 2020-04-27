f1a84daa070f30c7d638f59415b672784e876eca
zhiyong.li <zhiyong.li@streamcomputing.com>
2020-04-27 10:35:02 +0800

    fix viota function error.

--- riscv/insns/viota_m.h
+++ riscv/insns/viota_m.h
@@ -1,6 +1,11 @@
 require_extension('V');
 // FIXME: should implement ncp access trap
 
+if (unlikely((insn.rd() == insn.rs1())
+	|| (insn.rd() == insn.rs2())
+	|| (insn.vm() == 0 &&(insn.rd() == 0)))) {
+	throw trap_ncp_rvv_invalid_same_rdrs();
+	}
 int sum = 0;
 vector_for_each_from_zero(idx) {
 	switch(SEW) {
@@ -13,9 +18,9 @@ vector_for_each_from_zero(idx) {
 			    WRITE_VRD_B(0, idx);
 			}
 			else {
-			    sum += (v0b_mask(idx) ? 0 : (VRS2.vb[idx-1] & 0x1));
 			    WRITE_VRD_B((char)sum, idx);
 			}
+			sum += VRS2.vb[idx] & 0x1;
 		}
 		else {
 			    WRITE_VRD_B((char)0, idx);
@@ -25,16 +30,16 @@ vector_for_each_from_zero(idx) {
 
 	case 16:
 		if (idx < VL) {
-			if (idx == 0) {
+			if (v0h_mask(idx)) {
+				continue;
+			}
+			else if (idx == 0) {
 			    WRITE_VRD_H(0, idx);
 			}
-			else if (v0h_mask(idx)) {
-			    continue;
-			}
 			else {
-			    sum += (v0h_mask(idx) ? 0 : (VRS2.vh[idx-1] & 0x1));
 			    WRITE_VRD_H((short)sum, idx);
 			}
+			sum += VRS2.vh[idx] & 0x1;
 		}
 		else {
 			    WRITE_VRD_H((short)0, idx);
