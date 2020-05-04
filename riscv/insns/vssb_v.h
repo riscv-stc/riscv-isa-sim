cf4016c12bc05c1ad0c2abc1b91dcbb684dfd658
Xin Ouyang <xin.ouyang@streamcomputing.com>
2020-05-04 11:20:10 +0800

    add exception check for vss*.v insts.

--- riscv/insns/vssb_v.h
+++ riscv/insns/vssb_v.h
@@ -1,8 +1,12 @@
 require_extension('V');
-// FIXME: should implement ncp access trap
+
 check_vstart{
 	switch(SEW){
 	case 8:
+		check_rvv_misaligned_base(RS1, int8)
+		check_rvv_misaligned_offset(RS2, int8)
+		check_rvv_access(RS1, VL * RS2 * SEW/8)
+
 		vector_for_each(idx){
 			check_v0bmask(idx);
 			MMU.store_uint8(RS1+idx*RS2, VRS3.vb[idx]);
