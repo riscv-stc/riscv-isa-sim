842e186b047b380c013f9fe02a925d80a8ef0a7e
hao.chen <haohaohao1991@163.com>
2020-05-18 11:59:48 +0800

    vlxbu_v trap order

--- riscv/insns/vlxbu_v.h
+++ riscv/insns/vlxbu_v.h
@@ -6,10 +6,9 @@ check_vstart{
 		check_rvv_misaligned_base(RS1, int8)
 
 		vector_for_each(idx){
-			check_v0bmask(idx);
-
 			check_rvv_misaligned_offset(VRS2.vh[idx], int8)
 			check_rvv_access(RS1+VRS2.vh[idx], SEW/8)
+            check_v0bmask(idx);
 
 			WRITE_VRD_H((uint16_t)MMU.load_uint8(RS1+VRS2.vh[idx]), idx);
 		}
