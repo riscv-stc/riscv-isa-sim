411a240546bf693be358a5a244e3bab5f43a3f61
hao.chen <haohaohao1991@163.com>
2020-05-18 12:09:56 +0800

    change trap order for vlxb_v, vlxe_v, vlxh_v, vlxhu_v, vsxb_v, vsxe_v, vsxh_v

--- riscv/insns/vlxhu_v.h
+++ riscv/insns/vlxhu_v.h
@@ -6,10 +6,9 @@ check_vstart{
 		check_rvv_misaligned_base(RS1, int16)
 
 		vector_for_each(idx){
-			check_v0hmask(idx);
-
 			check_rvv_misaligned_offset(VRS2.vh[idx], int16)
 			check_rvv_access(RS1+VRS2.vh[idx], SEW/8)
+            check_v0hmask(idx);
 
 			WRITE_VRD_H(MMU.load_uint16(RS1+VRS2.vh[idx]), idx);
 		}
--- riscv/insns/vsxb_v.h
+++ riscv/insns/vsxb_v.h
@@ -6,10 +6,9 @@ check_vstart{
 		check_rvv_misaligned_base(RS1, int8)
 
 		vector_for_each(idx){
-			check_v0bmask(idx);
-
 			check_rvv_misaligned_offset(VRS2.vh[idx], int8)
 			check_rvv_access(RS1+VRS2.vh[idx], SEW/8)
+			check_v0bmask(idx);
 
 			MMU.store_uint8(RS1+VRS2.vb[idx], VRS3.vb[idx]);
 		}
