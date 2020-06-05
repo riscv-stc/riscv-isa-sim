152813477a3fbb644c52331138a251bd81df0622
kai.ren <kai.ren@streamcomputing.com>
2020-06-05 16:10:23 +0800

    velut accesscheck fix

--- riscv/insns/velut_m.h
+++ riscv/insns/velut_m.h
@@ -9,4 +9,4 @@ unsigned long rs1 = MMU.get_phy_addr(RS1);
 unsigned long rs2 = MMU.get_phy_addr(RS2);
 unsigned long rd = MMU.get_phy_addr(RD);
 
-CusIns.velut_m((uint16_t*)rs1, (uint16_t*)rs2, (half*)rd, &sst);
+CusIns.velut_m((uint16_t*)rs1, rs2, (half*)rd, &sst);
