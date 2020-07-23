fe7d36416d36312a46fd601c2b9ebf3d00a05e00
Lei Liu <lei.liu@streamcomputing.com>
2020-07-23 12:44:22 +0800

    remove workaround for old TVM runtime performance
    Revert "set affinity for performance"
    
    This reverts commit 788ea7ffa4c068ada61fbaa3bf1958f8672b1d1c.

--- riscv/processor.cc
+++ riscv/processor.cc
@@ -17,10 +17,6 @@
 #include <stdexcept>
 #include <algorithm>
 
-#define __USE_GNU
-#include <sched.h>
-#include <pthread.h>
-
 #undef STATE
 #define STATE state
 
@@ -45,16 +41,7 @@ processor_t::processor_t(const char* isa, simif_t* sim, hwsync_t* hs,
   reset();
 
   // start a thread for receive async tasks
-  std::thread([this, id]() {
-    // set affinity for performance
-    cpu_set_t cpuset;
-    CPU_ZERO(&cpuset);
-    CPU_SET(id, &cpuset);
-    int rc = pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset);
-    if (rc != 0) {
-      std::cerr << "Error calling pthread_setaffinity_np: " << rc << "\n";
-    }
-
+  std::thread([this]() {
     while (true) {
       std::unique_lock<std::mutex> lock(async_mutex);
       async_cond.wait(lock, [this]{
