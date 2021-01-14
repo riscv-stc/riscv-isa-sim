d517da931cdaddd18e6d71cd770111d9e16bd990
Kening.Zhang <Kening.Zhang@streamcomputing.com>
2021-01-14 09:39:57 +0800

    fixes the data length of icmov
    length=0 for 512KB

--- riscv/insns/icmov.h
+++ riscv/insns/icmov.h
@@ -3,7 +3,7 @@ check_traps_icmov;
 auto chipId = DST_CHIP_ID;
 auto coreId = DST_CORE_ID;
 
-auto dataSize = RS2;
+auto dataSize = (RS2==0)? (1024*512): RS2;
 
 auto src = (uint8_t*)MMU.get_phy_addr(RS1);
 auto dst = (uint8_t*)p->get_sim()->local_addr_to_mem_by_id_cluster(zext_xlen(RD), coreId);
