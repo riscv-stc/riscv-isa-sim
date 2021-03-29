3f288ac989b5c15954ac23bf3b0cd855760e3c02
Tianliang.Nan <Tianliang.Nan@streamcomputing.com>
2021-03-29 17:00:54 +0800

    fix velut ins sst_fill

--- riscv/insns/velut_m.h
+++ riscv/insns/velut_m.h
@@ -3,7 +3,7 @@ check_ncp_vill_invalid()
 check_traps_velut_m;
 
 struct ShapeStride sst;
-sst_fill(sst, 2, 2);
+sst_fill(sst, 1, 1);
 unsigned long rs1 = MMU.get_phy_addr(RS1);
 unsigned long rs2 = MMU.get_phy_addr(RS2);
 unsigned long rd = MMU.get_phy_addr(RD);
