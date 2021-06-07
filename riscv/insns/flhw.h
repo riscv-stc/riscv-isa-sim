9e4dd7e75806d660f32d0bac1d57d2f63cc1bf7e
kening.zhang <kening.zhang@streamcomputing.com>
2021-06-07 10:16:39 +0800

    fixed the imm to 0 of flhw/fshw
    Updated SIM-291

--- riscv/insns/flhw.h
+++ riscv/insns/flhw.h
@@ -1,5 +1,6 @@
 require_extension('F');
 require_fp;
 float16_t f16;
-f16.v = MMU.load_uint16(RS1 + insn.i_imm());
+assert(insn.i_imm() == 0);
+f16.v = MMU.load_uint16(RS1);
 WRITE_FRD(f16_to_f32(f16));
--- riscv/insns/fshw.h
+++ riscv/insns/fshw.h
@@ -1,3 +1,4 @@
 require_extension('F');
 require_fp;
-MMU.store_uint16(RS1 + insn.s_imm(), f32_to_f16(f32(FRS2.v[0])).v);
+assert(insn.s_imm() == 0);
+MMU.store_uint16(RS1, f32_to_f16(f32(FRS2.v[0])).v);
