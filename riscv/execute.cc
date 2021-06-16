558a1612075cf58c7b25a279461d47b034b7d159
Tianliang.Nan <Tianliang.Nan@streamcomputing.com>
2021-06-16 17:06:19 +0800

    add sleep when in wfi state

--- riscv/execute.cc
+++ riscv/execute.cc
@@ -171,8 +171,11 @@ void processor_t::step(size_t n)
 
      /* check current core wfi state, if it is in wfi state, swap current core to idle,
       * and let other core to execute insn. */
-      if (unlikely(state.wfi_flag))
+      if (unlikely(state.wfi_flag)) {
+        //sleep 100ms in wfi
+        usleep(100000);
         break;
+      }
 
       if (unlikely(slow_path()))
       {
