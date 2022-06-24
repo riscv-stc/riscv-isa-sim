252d54db414440527db669ac6d7a18a23c4573f8
menglong.hu <menglong.hu@streamcomputing.com>
2022-06-24 17:44:45 +0800

    implement 3 registers:sync_group_mask,group_valid,sync_status;implement group_vaild register virtualization

--- riscv/hwsync.cc
+++ riscv/hwsync.cc
@@ -12,147 +12,93 @@
 
 //#define DEBUG
 
-hwsync_t::hwsync_t(char *hwsync_masks, uint32_t hwsync_timer_num) : group_count(16), 
-        core_num(32), hwsync_mem_size(512) {
+hwsync_t::hwsync_t(char *hwsync_masks, uint32_t hwsync_timer_num) : group_count(16),
+                                                                    core_num(32), hwsync_mem_size(512)
+{
+    hwsync_base_addr = new uint8_t[HWSYNC_SIZE];
+    memset(hwsync_base_addr, 0, HWSYNC_SIZE);
+    sync_masks = (uint32_t *)(hwsync_base_addr + GROUP_MASK_OFFSET);
+    group_done = (uint32_t *)(hwsync_base_addr + GROUP_DONE_OFFSET);
+    group_valid = (uint32_t *)(hwsync_base_addr + GROUP_VALID_OFFSET);
+    sync_status = (uint32_t *)(hwsync_base_addr + SYNC_STATUS_OFFSET);
+    group_locks = new std::condition_variable_any[group_count];
 
-    hwsync_shm_ptr = (char *)malloc(hwsync_mem_size);
-    if (hwsync_shm_ptr == nullptr)
-        throw std::runtime_error("hwsync mmap failed");
-    memset(hwsync_shm_ptr,0,hwsync_mem_size);
+    hwsync_ptr = hwsync_base_addr;
+    req_pld = new uint32_t(~0);
 
-    req_sync = (uint32_t *)hwsync_shm_ptr;
-    hwsync_shm_ptr += 4;
-
-    req_pld = (uint32_t *)hwsync_shm_ptr;
-    hwsync_shm_ptr += 4;
-
-    masks = (uint32_t *)hwsync_shm_ptr;
-    hwsync_shm_ptr += 64;
-#ifdef HWSYNC_USED_PMUTEX
-    pmutex_pld = (pthread_mutex_t *)hwsync_shm_ptr;
-    hwsync_shm_ptr += sizeof(pthread_mutex_t);     /* __SIZEOF_PTHREAD_MUTEX_T==40 */
-
-    pmutex_pld = (pthread_mutex_t *)hwsync_shm_ptr;
-    hwsync_shm_ptr += sizeof(pthread_mutex_t);     /* __SIZEOF_PTHREAD_MUTEX_T==40 */
-
-    pmutex_sync = (pthread_mutex_t *)hwsync_shm_ptr;
-    hwsync_shm_ptr += sizeof(pthread_mutex_t);
-
-    pcond_pld = (pthread_cond_t *)hwsync_shm_ptr;
-    hwsync_shm_ptr += sizeof(pthread_cond_t);     /* __SIZEOF_PTHREAD_COND_T==48 */
-
-    pcond_sync = (pthread_cond_t *)hwsync_shm_ptr;
-    hwsync_shm_ptr += sizeof(pthread_cond_t);
-#endif
-    hs_sync_timer_num = (decltype(hs_sync_timer_num))hwsync_shm_ptr;
-    hwsync_shm_ptr += sizeof(*hs_sync_timer_num);
-
-    hs_sync_timer_cnt = (decltype(hs_sync_timer_cnt))hwsync_shm_ptr;
-    hwsync_shm_ptr += sizeof(*hs_sync_timer_cnt) * core_num;
-
+    hs_sync_timer_num = new uint32_t(0);
     if (0 == hwsync_timer_num)
         *hs_sync_timer_num = ~0;
-    else 
+    else
         *hs_sync_timer_num = hwsync_timer_num;
 
-    for (int i = 0 ; i < core_num ; i++) {
-        hs_sync_timer_cnt[i] = 0;
-    }
+    hs_sync_timer_cnt = new uint32_t[core_num];
+    memset(hs_sync_timer_cnt, 0, core_num);
+    memset(sync_masks, MASK_INITIAL_VALUE, group_count * sizeof(uint32_t));
 
-    for (int i = 0; i < group_count; i++)
-        masks[i] = ~0;
-
-    *req_sync = ~0;
-    *req_pld = ~0;
-
-    if (hwsync_masks[0] != 0) {
+    if (hwsync_masks[0] != 0)
+    {
         uint8_t index = 0;
         char *p = NULL;
         const char *delim = ",";
-    #ifdef HWSYNC_USED_PMUTEX
-        pthread_mutexattr_t attrmutex_pld;
-        pthread_mutexattr_t attrmutex_sync;
-        pthread_condattr_t attrcond_pld;
-        pthread_condattr_t attrcond_sync;
-
-        pthread_mutexattr_init(&attrmutex_pld);
-        pthread_mutexattr_setpshared(&attrmutex_pld, PTHREAD_PROCESS_SHARED);
-        pthread_mutex_init(pmutex_pld, &attrmutex_pld);
-
-        pthread_mutexattr_init(&attrmutex_sync);
-        pthread_mutexattr_setpshared(&attrmutex_sync, PTHREAD_PROCESS_SHARED);
-        pthread_mutex_init(pmutex_sync, &attrmutex_sync);
-
-        pthread_condattr_init(&attrcond_pld);
-        pthread_condattr_setpshared(&attrcond_pld, PTHREAD_PROCESS_SHARED);
-        pthread_cond_init(pcond_pld, &attrcond_pld);
-
-        pthread_condattr_init(&attrcond_sync);
-        pthread_condattr_setpshared(&attrcond_sync, PTHREAD_PROCESS_SHARED);
-        pthread_cond_init(pcond_sync, &attrcond_sync);
-    #endif
         // add all processors to group
         p = std::strtok(hwsync_masks, delim);
-        if (p) {
-            while (p) {
-                masks[index++] = std::stoul(p, nullptr, 16);
+        if (p)
+        {
+            while (p)
+            {
+                sync_masks[index++] = std::stoul(p, nullptr, 16);
                 p = std::strtok(NULL, delim);
                 if (index >= group_count)
                     break;
             }
-        } else {
+        }
+        else
+        {
             throw std::runtime_error("hwsync_masks error");
         }
-    } else {
+    }
+    else
+    {
         throw std::runtime_error("hwsync_masks error");
     }
 }
 
-hwsync_t::~hwsync_t() {
-    *req_sync = ~0;
-#ifdef HWSYNC_USED_PMUTEX
-    pthread_cond_broadcast(pcond_sync);
-#else
+hwsync_t::~hwsync_t()
+{
+    *sync_status = 0;
     cond_sync.notify_all();
-#endif
 }
 
-bool
-hwsync_t::enter(unsigned core_id) {
+bool hwsync_t::enter(unsigned core_id)
+{
 #ifdef DEBUG
     std::cout << "core" << core_id << ": start sync" << std::endl;
 #endif
-#ifdef HWSYNC_USED_PMUTEX
-        pthread_mutex_lock(pmutex_sync);
-        *req_sync &= ~(1 << core_id);
-        for (int i = 0; i < group_count; i++) {
-            if (((*req_sync | masks[i]) == masks[i]) && (~masks[i] != 0)) {
-                // all enter, clear enter requests
-                *req_sync |= ~masks[i];
-                pthread_cond_broadcast(pcond_sync);
-                break;
-            }
-        }
-        pthread_mutex_unlock(pmutex_sync);
 
-        pthread_mutex_lock(pmutex_sync);
-        while ((*req_sync & 1 << core_id) == 0)
-            pthread_cond_wait(pcond_sync, pmutex_sync);
-        pthread_mutex_unlock(pmutex_sync);
-#else
-        std::unique_lock<std::mutex> lock(mutex_sync);
-        *req_sync &= ~(1 << core_id);
-        for (int i=0; i< group_count; i++) {
-            if (((*req_sync | masks[i]) == masks[i]) && (~masks[i] != 0)) {
-                // all enter, clear enter requests
-                *req_sync |= ~masks[i];
-                cond_sync.notify_all();
-                break;
-            }
+    std::unique_lock<std::mutex> lock(mutex_sync);
+    setBitValue(*sync_status, core_id, 1);
+    for (int idx = 0; idx < group_count; idx++)
+    {
+        bool valid = getBitValue(*group_valid, idx);
+        if (valid && (sync_masks[idx] != MASK_INITIAL_VALUE) && is_group_all_sync(idx))
+        {
+            // all enter, clear enter requests
+            *sync_status &= sync_masks[idx];
+            group_locks[idx].notify_all();
+            break;
         }
+    }
 
-        cond_sync.wait(lock, [&]{ return (*req_sync & 1 << core_id) != 0; });
-#endif
+    int group_id = get_groupID_from_coreID(core_id);
+    bool valid = getBitValue(*group_valid, group_id);
+    // if the core is not assigned to any group or register GROUP_VALID is fasle,just jump
+    if (group_id >= 0 && valid)
+    {
+        // if the the number <group_id> bit of register sync_status is 1,just wait,if it is 0,just pass
+        group_locks[group_id].wait(lock, [&]
+                                   { return getBitValue(*sync_status, core_id) == 0; });
+    }
 
 #ifdef DEBUG
     std::cout << "core" << core_id << ": end sync" << std::endl;
@@ -161,37 +107,22 @@ hwsync_t::enter(unsigned core_id) {
     return true;
 }
 
-bool
-hwsync_t::enter(unsigned core_id, uint32_t coremap) {
+bool hwsync_t::enter(unsigned core_id, uint32_t coremap)
+{
 #ifdef DEBUG
     std::cout << "core" << core_id << ": start pld, coremap=" << coremap << std::endl;
 #endif
-#ifdef HWSYNC_USED_PMUTEX
-        pthread_mutex_lock(pmutex_pld);
-        *req_pld &= ~(1 << core_id);
+    std::unique_lock<std::mutex> lock(mutex_pld);
+    *req_pld &= ~(1 << core_id);
+    if ((*req_pld | ~coremap) == ~coremap)
+    {
+        // all enter, clear enter requests
+        *req_pld |= coremap;
+        cond_pld.notify_all();
+    }
+    cond_pld.wait(lock, [&]
+                  { return (*req_pld & 1 << core_id) != 0; });
 
-        if ((*req_pld | ~coremap) == ~coremap) {
-            // all enter, clear enter requests
-            *req_pld |= coremap;
-            pthread_cond_broadcast(pcond_pld);
-        }
-        pthread_mutex_unlock(pmutex_pld);
-
-        pthread_mutex_lock(pmutex_pld);
-        while ((*req_pld & 1 << core_id) == 0)
-            pthread_cond_wait(pcond_pld, pmutex_pld);
-        pthread_mutex_unlock(pmutex_pld);
-#else
-        std::unique_lock<std::mutex> lock(mutex_pld);
-        *req_pld &= ~(1 << core_id);
-        if ((*req_pld | ~coremap) == ~coremap) {
-            // all enter, clear enter requests
-            *req_pld |= coremap;
-            cond_pld.notify_all();
-        }
-        cond_pld.wait(lock, [&]{ return (*req_pld & 1 << core_id) != 0; });
-#endif
-    
 #ifdef DEBUG
     std::cout << "core" << core_id << ": end pld" << std::endl;
 #endif
@@ -199,34 +130,27 @@ hwsync_t::enter(unsigned core_id, uint32_t coremap) {
     return true;
 }
 
+bool hwsync_t::is_group_all_sync(int group_id)
+{
+    if ((~(*sync_status) | sync_masks[group_id]) == sync_masks[group_id])
+        return true;
+    return false;
+}
+
 bool hwsync_t::is_hs_group_sync(int coreid)
 {
     int grp = 0;
     int coren = 0;
 
-    for (grp = 0 ; grp < group_count; grp++) {
-        if (0 == ((1<<coreid) & masks[grp])) {
-            for (coren = 0; coren < 32 ; coren++) {
-                if (0 == ((1<<coren) & (masks[grp] | *req_sync)))
+    for (grp = 0; grp < group_count; grp++)
+    {
+        if (0 == ((1 << coreid) & sync_masks[grp]))
+        {
+            for (coren = 0; coren < 32; coren++)
+            {
+                if (0 == ((1 << coren) & (sync_masks[grp] | ~(*sync_status))))
                     return true;
             }
-        }
-    }
-
-    return false;
-}
-
-/* sync是否完成， *req_sync值为全f 或 grp内所有核都sync判定sync完成 */
-bool hwsync_t::is_hwsync_done(void)
-{
-    int i = 0;
-
-    if (likely(0xffffffff == *req_sync)) {
-        return true;
-    } else {
-        for (i = 0 ; i < group_count; i++) {
-            if (*req_sync == masks[i])
-                return true;
         }
     }
 
@@ -239,14 +163,18 @@ bool hwsync_t::is_hwsync_timeout(int coreid)
     int grpn = 0;
     int coren = 0;
 
-    if ((core_num<=coreid) || (NULL==hs_sync_timer_cnt))
+    if ((core_num <= coreid) || (NULL == hs_sync_timer_cnt))
         return false;
 
-    for (grpn = 0 ; grpn < group_count; grpn++) {
-        if (0 == ((1<<coreid) & masks[grpn])) {
-            for (coren = 0; coren < core_num ; coren++) {
+    for (grpn = 0; grpn < group_count; grpn++)
+    {
+        if (0 == ((1 << coreid) & sync_masks[grpn]))
+        {
+            for (coren = 0; coren < core_num; coren++)
+            {
                 if ((get_hwsync_timer_cnts(coren) > get_hwsync_timer_thresh()) &&
-                    (0 == ((1<<coren) & (masks[grpn])))) {
+                    (0 == ((1 << coren) & (sync_masks[grpn]))))
+                {
                     return true;
                 }
             }
@@ -260,9 +188,9 @@ void hwsync_t::hwsync_timer_cnts_add(int coreid, uint32_t clks)
 {
     uint64_t tmp = 0;
 
-    if ((core_num<=coreid) || (NULL==hs_sync_timer_cnt))
+    if ((core_num <= coreid) || (NULL == hs_sync_timer_cnt))
         return;
-    
+
     tmp = (uint64_t)(hs_sync_timer_cnt[coreid]) + clks;
     if (0xffffffff < tmp)
         hs_sync_timer_cnt[coreid] = 0xffffffff;
@@ -272,7 +200,7 @@ void hwsync_t::hwsync_timer_cnts_add(int coreid, uint32_t clks)
 
 uint32_t hwsync_t::get_hwsync_timer_cnts(int coreid)
 {
-    if ((core_num<=coreid) || (NULL==hs_sync_timer_cnt))
+    if ((core_num <= coreid) || (NULL == hs_sync_timer_cnt))
         return 0;
 
     return hs_sync_timer_cnt[coreid];
@@ -280,7 +208,7 @@ uint32_t hwsync_t::get_hwsync_timer_cnts(int coreid)
 
 void hwsync_t::hwsync_timer_clear(int coreid)
 {
-    if ((core_num<=coreid) || (NULL==hs_sync_timer_cnt))
+    if ((core_num <= coreid) || (NULL == hs_sync_timer_cnt))
         return;
     hs_sync_timer_cnt[coreid] = 0;
 }
@@ -289,34 +217,91 @@ void hwsync_t::hwsync_clear(void)
 {
     int i = 0;
 
-    *req_sync = ~0;
-#ifdef HWSYNC_USED_PMUTEX
-    pthread_cond_broadcast(pcond_sync);
-#else
+    *sync_status = 0;
     cond_sync.notify_all();
-#endif
 
-    for (i = 0 ; i < core_num ; i++) {
+    for(int i = 0; i < group_count; i++)
+    {
+        group_locks[i].notify_all();
+    }
+
+    for (i = 0; i < core_num; i++)
+    {
         hwsync_timer_clear(i);
     }
 }
 
-bool hwsync_t::load(reg_t addr, size_t len, uint8_t* bytes)
+bool hwsync_t::load(reg_t addr, size_t len, uint8_t *bytes)
 {
-  if (unlikely(!bytes || addr >= 4 * 16))
-    return false;
+    if (unlikely(!bytes || addr + len >= HWSYNC_SIZE))
+        return false;
 
-  *((uint32_t*)bytes) = masks[addr / 4];
+    uint8_t *now_addr = (uint8_t *)(hwsync_base_addr + addr);
+    memcpy(bytes, now_addr, len);
+    switch (addr)
+    {
+    // case GROUP_DONE_OFFSET:
+    case GROUP_VALID_OFFSET:
+        memset(bytes + 2, 0, 2); // set high 2 bytes as 0
+        break;
 
-  return true;
+    default:
+        break;
+    }
+
+    return true;
 }
 
-bool hwsync_t::store(reg_t addr, size_t len, const uint8_t* bytes)
+bool hwsync_t::store(reg_t addr, size_t len, const uint8_t *bytes)
 {
-  if (unlikely(!bytes || addr >= 4 * 16))
-    return false;
+    if (unlikely(!bytes || addr + len >= HWSYNC_SIZE))
+        return false;
 
-  masks[addr / 4] = *((uint32_t*)bytes);
+    uint8_t *now_addr = (uint8_t *)(hwsync_base_addr + addr);
+    switch (addr)
+    {
+    case SYNC_STATUS_OFFSET:
+        break;
+    // case GROUP_DONE_OFFSET:
+    //     memcpy(hwsync_base_addr + addr, bytes, len > 2 ? 2 : len);
+    //     break;
+    case GROUP_VALID_OFFSET:
+        memcpy(now_addr, bytes, len > 2 ? 2 : len);
+        cpyBits(now_addr, hwsync_base_addr + GROUP_VALID_3_0, 0, 0, 4);
+        cpyBits(now_addr, hwsync_base_addr + GROUP_VALID_7_4, 4, 0, 4);
+        cpyBits(now_addr + 1, hwsync_base_addr + GROUP_VALID_11_8, 0, 0, 4);
+        cpyBits(now_addr + 1, hwsync_base_addr + GROUP_VALID_15_11, 4, 0, 4);
+        break;
+    case GROUP_VALID_3_0:
+        memcpy(hwsync_base_addr + addr, bytes, len);
+        cpyBits(hwsync_base_addr + GROUP_VALID_3_0, now_addr, 0, 0, 4);
+        break;
+    case GROUP_VALID_7_4:
+        memcpy(hwsync_base_addr + addr, bytes, len);
+        cpyBits(hwsync_base_addr + GROUP_VALID_7_4, now_addr, 0, 4, 4);
+        break;
+    case GROUP_VALID_11_8:
+        memcpy(hwsync_base_addr + addr, bytes, len);
+        cpyBits(hwsync_base_addr + GROUP_VALID_11_8, now_addr + 1, 0, 0, 4);
+        break;
+    case GROUP_VALID_15_11:
+        memcpy(hwsync_base_addr + addr, bytes, len);
+        cpyBits(hwsync_base_addr + GROUP_VALID_15_11, now_addr + 1, 0, 4, 4);
+        break;
+    default:
+        memcpy(hwsync_base_addr + addr, bytes, len);
+        break;
+    }
 
-  return true;
+    return true;
+}
+
+uint32_t hwsync_t::get_groupID_from_coreID(uint32_t core_id)
+{
+    for (int i = 0; i < group_count; i++)
+    {
+        if (getBitValue(sync_masks[i], core_id) == 0)
+            return i;
+    }
+    return -1;
 }
--- riscv/hwsync.h
+++ riscv/hwsync.h
@@ -7,31 +7,47 @@
 #include "decode.h"
 #include <semaphore.h>
 #include <pthread.h>
+#include <bitset>
 
 #include "devices.h"
 
-//#define HWSYNC_USED_PMUTEX
+#define MASK_INITIAL_VALUE          (~0)
+
+#define setBitValue(var, idx, value) (var) = (value > 0) ? ((var) | (1 << (idx))) : ((var) & ~(1 << (idx)))
+#define clearBitMap(var, fromBit, toBit) (var) = ((var) & ~(((0x1 << ((toBit) + 1 - (fromBit))) - 1) << (fromBit)))
+#define getBitValue(var, idx) ((var >> (idx)) & 1)
 
 class hwsync_t: public abstract_device_t {
   private:
-    uint32_t *masks;      /* masks[0]-masks[15]共16个sync组, 为0的bit对应的核在同一个sync组， 默认值{0xffffffff} */
-    uint32_t *req_sync;   /* core_n开始sync时第n位清零，sync结束重新置位, 初始值0xffffffff */
+    uint32_t *sync_masks;      /* sync_masks[0]-sync_masks[15]共16个sync组, 为0的bit对应的核在同一个sync组， 默认值{0xffffffff} */
+    uint32_t *sync_status;   /* core_n开始sync时第n位清零，sync结束重新, 初始值0x0000 */
+    uint32_t* group_valid;
+    uint32_t* group_done;
     uint32_t *req_pld;
     int group_count;  /* 支持16个sync组 */
     int core_num;     /* 支持32个核 */
     int hwsync_mem_size;
-    char *hwsync_shm_ptr;
-#ifdef HWSYNC_USED_PMUTEX
-    pthread_mutex_t * pmutex_pld;
-    pthread_mutex_t * pmutex_sync;
-    pthread_cond_t * pcond_pld;
-    pthread_cond_t * pcond_sync;
-#else
+
+    uint8_t *hwsync_ptr;
+    uint8_t* hwsync_base_addr;
+
+    std::condition_variable_any *group_locks;
+    
+    void cpyBits(uint8_t *src, uint8_t *dst, int src_start, int dst_start, int len)
+    {
+      for (int i = 0; i < len; i++)
+      {
+          setBitValue(*dst, dst_start + i, getBitValue(*src, src_start + i));
+      }
+    }
+    uint32_t get_groupID_from_coreID(uint32_t core_id);
+
+
     std::mutex mutex_sync;
     std::mutex mutex_pld;
     std::condition_variable_any cond_sync;
+    
     std::condition_variable_any cond_pld;
-#endif
     
     uint32_t *hs_sync_timer_num;  /* timeout阈值 HS_TIME_OUT_CNT，所有grp公用一个阈值 */
     uint32_t *hs_sync_timer_cnt;  /* 为每个核分配一个sync timer(硬件只有一个timer) */
@@ -47,24 +63,23 @@ class hwsync_t: public abstract_device_t {
     bool store(reg_t addr, size_t len, const uint8_t* bytes);
     bool is_hwsync_done(void);
     bool is_hs_group_sync(int coreid);   /* coreid所在的sync组是否处于sync状态 */
+    bool is_group_all_sync(int group_id); //Determines whether a group is fully synchronized
     bool is_hwsync_timeout(int coreid);
     void hwsync_timer_cnts_add(int coreid, uint32_t clks);
     void hwsync_timer_clear(int coreid);
     void hwsync_clear(void);
     uint32_t get_hwsync_timer_cnts(int coreid);
-    uint32_t get_hwsync() { return *req_sync;}
+    uint32_t get_hwsync() { return *sync_status;}
     uint32_t get_hwsync_timer_thresh(void) {
       return (hs_sync_timer_num) ? *hs_sync_timer_num : 0xffffffff;
     };
     void reset(uint32_t id) {
       *req_pld |= (0x1 << id);
-    #ifdef HWSYNC_USED_PMUTEX
-        pthread_cond_broadcast(pcond_pld);
-    #else
-        cond_pld.notify_all();
-    #endif
+      cond_pld.notify_all();
     }
     size_t size(void) {return 0x80000;};
+    
+   
 };
 
 #endif // __HWSYNC_H__
