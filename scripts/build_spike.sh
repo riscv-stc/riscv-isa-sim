44bbabf686170dd5c258159228c59bbcd8bc7268
Sun Xuesong <xuesong.sun@streamcomputing.com>
2023-01-12 20:18:00 +0800

    fix ci disable commitlog for ubuntu release

--- scripts/build_spike.sh
+++ scripts/build_spike.sh
@@ -9,5 +9,12 @@ fi
 BUILD_DIR="$1"
 
 mkdir -p $BUILD_DIR
-cd $BUILD_DIR && ../configure --with-isa=rv64gcv_zfh --with-varch=vlen:256,elen:32,slen:256 --enable-commitlog --enable-misaligned --prefix=$PWD/../install && make -j`nproc` && cd -
+
+# ubuntu的release 不使能commitlog 以提升性能
+if [[ $BUILD_DIR =~ "ubuntu" ]]
+then
+    cd $BUILD_DIR && ../configure --with-isa=rv64gcv_zfh --with-varch=vlen:256,elen:32,slen:256 --enable-misaligned --prefix=$PWD/../install && make -j`nproc` && cd -
+else
+    cd $BUILD_DIR && ../configure --with-isa=rv64gcv_zfh --with-varch=vlen:256,elen:32,slen:256 --enable-commitlog --enable-misaligned --prefix=$PWD/../install && make -j`nproc` && cd -
+fi
 
