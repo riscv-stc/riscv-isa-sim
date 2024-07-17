5ad1b4412adb87d9a9a86bc390c7b49675fbbbe9
Chaoqun.Wang <Chaoqun.Wang@streamcomputing.com>
2024-07-17 18:20:46 +0900

    fix mfcvt lmul

--- riscv/insns/mfcvt_x_f_m.h
+++ riscv/insns/mfcvt_x_f_m.h
@@ -4,14 +4,14 @@ MXU_MFP_CVT
     ;
 },
 {
-    auto ts1 = P.MU.tr_elt<float16_t>(ts1_num, 0, i, j, mmax, nmax, false, false);
+    auto ts1 = P.MU.tr_elt<float16_t>(ts1_num + m, 0, i, j, mmax, nmax, false, false);
     P.MU.tr_elt<int16_t>(td_num + m, 0, i, j, mmax, nmax, reg_rename, true) = f16_to_i16(ts1, softfloat_roundingMode, true);
 },
 {
-    auto ts1 = P.MU.tr_elt<float32_t>(ts1_num, 0, i, j, mmax, nmax, false, false);
+    auto ts1 = P.MU.tr_elt<float32_t>(ts1_num + m , 0, i, j, mmax, nmax, false, false);
     P.MU.tr_elt<int32_t>(td_num + m, 0, i, j, mmax, nmax, reg_rename, true) = f32_to_i32(ts1, softfloat_roundingMode, true);
 }, 
 {
-    auto ts1 = P.MU.tr_elt<float64_t>(ts1_num, 0, i, j, mmax, nmax, false, false);
+    auto ts1 = P.MU.tr_elt<float64_t>(ts1_num + m, 0, i, j, mmax, nmax, false, false);
     P.MU.tr_elt<int64_t>(td_num + m, 0, i, j, mmax, nmax, reg_rename, true) = f64_to_i64(ts1, softfloat_roundingMode, true);
 }, 1)
