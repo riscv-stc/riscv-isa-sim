2cc3bf8956d98fbfec3f7e5a49a0f983ff05eb3a
Chaoqun.Wang <Chaoqun.Wang@streamcomputing.com>
2024-07-19 12:20:43 +0900

    fix mfncvt lmul error

--- riscv/insns/mfncvt_fw_xq_m.h
+++ riscv/insns/mfncvt_fw_xq_m.h
@@ -10,7 +10,7 @@
   },
   {
     auto ts1 = P.MU.tr_elt<int64_t>(ts1_num + m, 0, i, j, mmax, nmax, false, false);
-    P.MU.tr_elt<float32_t>(td_num + m / 2, 0, i, j, mmax, nmax * 2, reg_rename, true) = i64_to_f32(ts1);
+    P.MU.tr_elt<float32_t>(td_num + m / 2, 0, i, j + tile_n * m, mmax, nmax * 2, reg_rename, true) = i64_to_f32(ts1);
   },
   {
     ;
