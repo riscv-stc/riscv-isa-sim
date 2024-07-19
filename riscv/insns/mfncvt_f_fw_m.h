7825f67b450ccab1ecc4797964e01c1cc62c0891
Chaoqun.Wang <Chaoqun.Wang@streamcomputing.com>
2024-07-19 16:17:45 +0900

    fix mfncvt lmul error

--- riscv/insns/mfncvt_f_fw_m.h
+++ riscv/insns/mfncvt_f_fw_m.h
@@ -7,11 +7,11 @@
   },
   {
     auto ts1 = P.MU.tr_elt<float32_t>(ts1_num + m, 0, i, j, mmax, nmax, false, false);
-    P.MU.tr_elt<float16_t>(td_num + m / 2, 0, i, j + tile_n * m, mmax, nmax * 2, reg_rename, true) = f32_to_f16(ts1);
+    P.MU.tr_elt<float16_t>(td_num + m / 2, 0, i, j + tile_n * (m % 2), mmax, nmax * 2, reg_rename, true) = f32_to_f16(ts1);
   },
   {
     auto ts1 = P.MU.tr_elt<float64_t>(ts1_num + m, 0, i, j, mmax, nmax, false, false);
-    P.MU.tr_elt<float32_t>(td_num + m / 2, 0, i, j + tile_n * m, mmax, nmax * 2, reg_rename, true) = f64_to_f32(ts1);
+    P.MU.tr_elt<float32_t>(td_num + m / 2, 0, i, j + tile_n * (m % 2), mmax, nmax * 2, reg_rename, true) = f64_to_f32(ts1);
   },
   {
     ;
--- riscv/insns/mfncvt_x_fw_m.h
+++ riscv/insns/mfncvt_x_fw_m.h
@@ -4,15 +4,15 @@
   },
     {
     auto ts1 = P.MU.tr_elt<float16_t>(ts1_num + m, 0, i, j, mmax, nmax, false, false);
-    P.MU.tr_elt<int8_t>(td_num + m / 2, 0, i, j + tile_n * m, mmax, nmax * 2, reg_rename, true) = f16_to_i8(ts1, softfloat_roundingMode, true);
+    P.MU.tr_elt<int8_t>(td_num + m / 2, 0, i, j + tile_n * (m % 2), mmax, nmax * 2, reg_rename, true) = f16_to_i8(ts1, softfloat_roundingMode, true);
   },
   {
     auto ts1 = P.MU.tr_elt<float32_t>(ts1_num + m, 0, i, j, mmax, nmax, false, false);
-    P.MU.tr_elt<int16_t>(td_num + m / 2, 0, i, j + tile_n * m, mmax, nmax * 2, reg_rename, true) = f32_to_i16(ts1, softfloat_roundingMode, true);
+    P.MU.tr_elt<int16_t>(td_num + m / 2, 0, i, j + tile_n * (m % 2), mmax, nmax * 2, reg_rename, true) = f32_to_i16(ts1, softfloat_roundingMode, true);
   },
   {
     auto ts1 = P.MU.tr_elt<float64_t>(ts1_num + m, 0, i, j, mmax, nmax, false, false);
-    P.MU.tr_elt<int32_t>(td_num + m / 2, 0, i, j + tile_n * m, mmax, nmax * 2, reg_rename, true) = f64_to_i32(ts1, softfloat_roundingMode, true);
+    P.MU.tr_elt<int32_t>(td_num + m / 2, 0, i, j + tile_n * (m % 2), mmax, nmax * 2, reg_rename, true) = f64_to_i32(ts1, softfloat_roundingMode, true);
   },
   {
     ;
