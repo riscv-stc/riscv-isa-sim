afe4867ab3b39a227b0fd88f0399317264118f9b
Chaoqun.Wang <Chaoqun.Wang@streamcomputing.com>
2024-10-08 19:03:00 +0900

    fix mfwmul load date from tr reg error

--- riscv/insns/mfwmul_cf_mm.h
+++ riscv/insns/mfwmul_cf_mm.h
@@ -40,10 +40,10 @@ MI_VFP_MM_LOOP(
 },
 {
     if (P.MU.mfp32 == MTYPE_FP32) {
-      float32_t &td_w = P.MU.tr_elt<float32_t>(td_num + m, 0, i, j, mmax, des_nmax * amul * wide, reg_rename, true); \
+      float32_t &td_w = P.MU.acc_elt<float32_t>(td_num + m, 0, i, j, mmax, des_nmax * amul * wide, reg_rename, true); \
       td_w = f32_mul(f16_to_f32(ts1), f16_to_f32(ts2));
     } else if (P.MU.mfp32 == MTYPE_TFP32){
-      tfloat32_t &td_w = P.MU.tr_elt<tfloat32_t>(td_num + m, 0, i, j, mmax, des_nmax * amul * wide, reg_rename, true); \
+      tfloat32_t &td_w = P.MU.acc_elt<tfloat32_t>(td_num + m, 0, i, j, mmax, des_nmax * amul * wide, reg_rename, true); \
       td_w = tf32_mul(f16_to_tf32(ts1), f16_to_tf32(ts2));
     } else {
       require(0);
@@ -51,21 +51,21 @@ MI_VFP_MM_LOOP(
 } ,
 {
     if (P.MU.mfp32 == MTYPE_FP32) {
-      float32_t &td_w = P.MU.tr_elt<float32_t>(td_num + m, 0, i, j, mmax, des_nmax * amul * wide, reg_rename, true); \
+      float32_t &td_w = P.MU.acc_elt<float32_t>(td_num + m, 0, i, j, mmax, des_nmax * amul * wide, reg_rename, true); \
       td_w = f32_mul(bf16_to_f32(ts1), bf16_to_f32(ts2));
     } else if (P.MU.mfp32 == MTYPE_TFP32){
-      tfloat32_t &td_w = P.MU.tr_elt<tfloat32_t>(td_num + m, 0, i, j, mmax, des_nmax * amul * wide, reg_rename, true); \
+      tfloat32_t &td_w = P.MU.acc_elt<tfloat32_t>(td_num + m, 0, i, j, mmax, des_nmax * amul * wide, reg_rename, true); \
       td_w = tf32_mul(bf16_to_tf32(ts1), bf16_to_tf32(ts2));
     } else {
       require(0);
     }
 } ,
 {
-    float64_t &td_w = P.MU.tr_elt<float64_t>(td_num + m, 0, i, j, mmax, des_nmax * amul * wide, reg_rename, true);
+    float64_t &td_w = P.MU.acc_elt<float64_t>(td_num + m, 0, i, j, mmax, des_nmax * amul * wide, reg_rename, true);
     td_w = f64_mul(f32_to_f64(ts1), f32_to_f64(ts2));
 },
 {
-    float64_t &td_w = P.MU.tr_elt<float64_t>(td_num + m, 0, i, j, mmax, des_nmax * amul * wide, reg_rename, true);
+    float64_t &td_w = P.MU.acc_elt<float64_t>(td_num + m, 0, i, j, mmax, des_nmax * amul * wide, reg_rename, true);
     td_w = f64_mul(tf32_to_f64(ts1), tf32_to_f64(ts2));
 },
 {
