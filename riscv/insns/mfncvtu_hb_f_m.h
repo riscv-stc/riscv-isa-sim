4f6fff8b47cba787de27e83a4c160d8742921302
Chaoqun.Wang <Chaoqun.Wang@streamcomputing.com>
2024-10-11 17:01:45 +0900

    fix mfncvt load date error

--- riscv/insns/mfncvtu_hb_f_m.h
+++ riscv/insns/mfncvtu_hb_f_m.h
@@ -25,7 +25,7 @@
         require(0);
     
     auto ts1 = P.MU.acc_elt<float64_t>(ts1_num + m, 0, i, j, mmax, nmax * amul, false, false);
-    P.MU.acc_elt<int8_t>(td_num + m, 0, i, j, mmax, nmax * amul * 2, reg_rename, true) = f64_to_i8(ts1, softfloat_roundingMode, true);
+    P.MU.acc_elt<int8_t>(td_num + m, 0, i, j, mmax, nmax * amul * 8, reg_rename, true) = f64_to_i8(ts1, softfloat_roundingMode, true);
   },
   {
     ;
